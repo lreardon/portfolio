0ee7640c92ca53750019bc10c3b77f83e9583b5e
Leland Reardon <leland6925@gmail.com>
2023-04-12 22:48:56 +0545

    delete slash

--- cfImageLoader.js
+++ cfImageLoader.js
@@ -1,5 +1,5 @@
 // cfImageLoader.js
 export default function cfImageLoader({ src, width, quality }) {
     const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-    return `https://www.landho.me/cdn-cgi/image/${params.join(',')}/${src}`
+    return `https://www.landho.me/cdn-cgi/image/${params.join(',')}${src}`
 }

197b31ba88e2ecb822e48772ba8e1b3c24981d4c
Leland Reardon <leland6925@gmail.com>
2023-04-12 14:05:59 +0545

    block color handling

--- src/components/PhotosBlock.jsx
+++ src/components/PhotosBlock.jsx
@@ -3,22 +3,26 @@ import Image from 'next/image'
 import Block from './Block';
 
 export default function PhotosBlock(props) {
+
+    const backgroundColor = props.twcssBackgroundColor
+    const frameColor = props.twcssFrameColor
+
     return (
-        <Block className='bg-orange-400'>
+        <Block className={`${backgroundColor}`}>
             <div className="flex flex-row w-full">
                 <div className='flex flex-col justify-center items-center w-1/2'>
                     <div className="flex flex-row w-3/4">
-                    <h1 className="bg-gray-400 text-3xl">
+                    <h1 className={`${frameColor} text-3xl`}>
                         From time to time I take photos, and I post my favorites <a className='text-purple-800' href="https://photos.landho.me" target="_blank">here</a>!
                     </h1>
                     </div>
                 </div>
-                <div className="flex flex-row w-full relative bg-orange-400 m-10">
+                <div className="flex flex-row w-full relative m-10">
                     <Image
                         src='/photos-block-image.jpg'
                         alt="A spotted leopard, in Yala National Park"
                         fill
-                        className="object-scale-down bg-gray-400 p-4"
+                        className={`object-scale-down ${frameColor} p-4`}
                         onClick={() => (window.open("https://photos.landho.me", '_blank', 'noreferrer'))}
                     />
                 </div>
--- src/components/WaterfallsBlock.jsx
+++ src/components/WaterfallsBlock.jsx
@@ -3,21 +3,25 @@ import Image from 'next/image'
 import Block from "./Block";
 
 export default function WaterfallsBlock(props) {
+
+    const backgroundColor = props.twcssBackgroundColor
+    const frameColor = props.twcssFrameColor
+
     return (
-        <Block className='bg-pink-400'>
+        <Block className={`${backgroundColor}`}>
             <div className="flex flex-row w-full justify-start">
                 <div className="flex flex-row w-1/2 relative m-10">
                     <Image
                         src='/waterfalls-block-image.jpeg'
                         alt="Waterfalls are the best!"
                         fill
-                        className="object-scale-down bg-blue-300 p-4"
+                        className={`object-scale-down ${frameColor} p-4`}
                         onClick={() => (window.open("https://waterfalls.landho.me", '_blank', 'noreferrer'))}
                     />
                 </div>
                 <div className='flex flex-col justify-center items-center w-1/2'>
                     <div className="flex flex-row w-3/4">
-                    <h1 className="bg-blue-300 text-3xl">
+                    <h1 className={`${frameColor} text-3xl`}>
                         I also have a <a className='text-purple-800' href="https://waterfalls.landho.me" target="_blank">blog</a> documenting my favorite waterfalls!
                     </h1>
                     </div>
